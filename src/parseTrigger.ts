885aadad9bcf14ec0f062830a9d8e9ec92a31d4f
钢琴家 <247076126@qq.com>
2021-12-19 23:36:01 +0800

    _

--- src/parseTrigger.ts
+++ src/parseTrigger.ts
@@ -1,12 +1,3 @@
-/**
- * 1. apply 定义为全局
- *   例如 apply_idCard
- *
- * 2. 可以为模块 前缀可以为任意,主要用于privde在某一个层级以内
- *  例如 一个组件和它的所有子组件想拥有一个scope作用域
- *      goods_isActiveButton
- *
- */
 
 import { isCustomChunk, isCustomParse, isString } from "./shared";
 import { GetProxy } from "./parsingGraph";
