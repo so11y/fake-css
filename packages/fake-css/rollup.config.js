84a6aa2bd1cee0d8c355dd343832765d19aecea8
钢琴家 <247076126@qq.com>
2021-12-23 23:17:57 +0800

    🔧 build: 添加prod打包

--- packages/fake-css/rollup.config.js
+++ packages/fake-css/rollup.config.js
@@ -1,22 +1,70 @@
-const typeScriptPlugin = require('rollup-plugin-typescript2');
+import typeScriptPlugin from 'rollup-plugin-typescript2';
+import rollupPluginReplace from '@rollup/plugin-replace';
+import path from 'path';
+import { terser } from 'rollup-plugin-terser';
+const pkg = require(path.resolve(__dirname, `package.json`));
 
 const banner = `/*!
-* $fake-css v0.0.4
-* (c) ${new Date().getFullYear()} zrrz
+* ${pkg.name} v${pkg.version}
+* (c) ${new Date().getFullYear()} ${pkg.author.name}
 * @license MIT
 */`;
 
-export default {
-	input: './src/fakeCss.ts',
-	plugins: [
-		typeScriptPlugin({
-			check: false
-		})
-	],
-	external: ['vue'],
-	output: {
-		banner,
-		file: './dist/fake-css.esm-browser.js',
-		format: 'es'
-	}
+const replaces = () => {
+	const replacesKey = {
+		__TEST__: false
+	};
+	Object.keys(replacesKey).forEach((key) => {
+		if (key in process.env) {
+			replacesKey[key] = process.env[key];
+		}
+	});
+	return rollupPluginReplace({
+		preventAssignment: true,
+		values: replacesKey
+	});
 };
+
+const createformat = (config) => {
+	const defaultConfig = Object.assign({ output: {}, plugins: [] }, config);
+	return {
+		input: './src/fakeCss.ts',
+		plugins: [
+			typeScriptPlugin({
+				check: false,
+				tsconfig: path.resolve(__dirname, './tsconfig.json'),
+				tsconfigOverride: {
+					exclude: ['./src/__tests__/**/*.ts']
+				}
+			}),
+			replaces(),
+			...defaultConfig.plugins
+		],
+		external: ['vue'],
+		output: defaultConfig.output
+	};
+};
+
+export default [
+	createformat({
+		output: {
+			banner,
+			file: './dist/fake-css.esm-browser.js',
+			format: 'es'
+		}
+	}),
+	createformat({
+		output: {
+			banner,
+			file: './dist/fake-css.prod.js',
+			format: 'cjs'
+		},
+		plugins: [
+			terser({
+				compress: {
+					ecma: 2015
+				}
+			})
+		]
+	})
+];
