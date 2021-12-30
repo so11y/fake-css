38ac841e87b1b73f64ba8885d318a9d7f205d922
sorry <247076126@qq.com>
2021-12-30 22:50:26 +0800

    Update vite.config.ts

--- packages/playground/vite.config.ts
+++ packages/playground/vite.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+	define: {
+	    __TEST__: 'true',
+	},
 	plugins: [
 		vue({
 			include: [/\.vue$/]
