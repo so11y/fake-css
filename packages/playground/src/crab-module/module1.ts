cae9b58222cc8bd7f99481c32b22b1d8c8c7fc18
钢琴家 <247076126@qq.com>
2022-01-03 23:29:31 +0800

    🐞 fix(训练场自定义模块路径):

--- packages/playground/src/crab-module/module1.ts
+++ packages/playground/src/crab-module/module1.ts
@@ -1,11 +1,11 @@
-import { defineModule } from "fake-css"
+import { defineModule } from "../../../fake-css/src/fakeCss"
 
 export default defineModule("user", {
     setup(v) {
         v.register("user_layout", {
             chunk(ref) {
                 return [
-                    ref.pl_30,
+                    ref.pl_20,
                     ref.pr_30,
                     ref.mt_15,
                     ref.mb_15,
